28843b15d11e1c6de2d9e891681b058e5e1aa3cb
Leonid Abasov <leonid.abasov@gmail.com>
2024-04-24 17:11:50 -0500

    feat: add postgre remove cosmos

--- terraform/environments/production/main.tf
+++ terraform/environments/production/main.tf
@@ -4,6 +4,8 @@ locals {
   resource_token               = substr(replace(lower(local.sha), "[^A-Za-z0-9_]", ""), 0, 13)
   api_command_line             = "gunicorn --workers 4 --threads 2 --timeout 60 --access-logfile \"-\" --error-logfile \"-\" --bind=0.0.0.0:8000 -k uvicorn.workers.UvicornWorker todo.app:app"
   cosmos_connection_string_key = "AZURE-COSMOS-CONNECTION-STRING"
+  pg_username                  = "AZURE-PG-USERNAME"
+  pg_password                  = "AZURE-PG-PASSWORD"
 }
 # ------------------------------------------------------------------------------------------------------
 # Deploy resource Group
@@ -59,22 +61,38 @@ module "keyvault" {
   access_policy_object_ids = [module.api.IDENTITY_PRINCIPAL_ID]
   secrets = [
     {
-      name  = local.cosmos_connection_string_key
-      value = module.cosmos.AZURE_COSMOS_CONNECTION_STRING
+      name  = local.pg_username
+      value = module.postgresql_server.AZURE_PG_USERNAME
+    },
+    {
+      name  = local.pg_password
+      value = module.postgresql_server.AZURE_PG_PASSWORD
     }
   ]
 }
 
 # ------------------------------------------------------------------------------------------------------
-# Deploy cosmos
+# Deploy postgresql server
 # ------------------------------------------------------------------------------------------------------
-module "cosmos" {
-  source         = "../../modules/cosmos"
+module "postgresql_server" {
+  source         = "../../modules/postgresql_server"
   location       = var.location
   rg_name        = azurerm_resource_group.rg.name
   tags           = azurerm_resource_group.rg.tags
   resource_token = local.resource_token
+  db_names       = ["BeSportSmart"]
 }
+
+# # ------------------------------------------------------------------------------------------------------
+# # Deploy cosmos
+# # ------------------------------------------------------------------------------------------------------
+# module "cosmos" {
+#   source         = "../../modules/cosmos"
+#   location       = var.location
+#   rg_name        = azurerm_resource_group.rg.name
+#   tags           = azurerm_resource_group.rg.tags
+#   resource_token = local.resource_token
+# }
 
 # ------------------------------------------------------------------------------------------------------
 # Deploy app service plan
@@ -122,7 +140,7 @@ module "api" {
   appservice_plan_id = module.appserviceplan.APPSERVICE_PLAN_ID
   app_settings = {
     "AZURE_COSMOS_CONNECTION_STRING_KEY"    = local.cosmos_connection_string_key
-    "AZURE_COSMOS_DATABASE_NAME"            = module.cosmos.AZURE_COSMOS_DATABASE_NAME
+    #"AZURE_COSMOS_DATABASE_NAME"            = module.cosmos.AZURE_COSMOS_DATABASE_NAME
     "SCM_DO_BUILD_DURING_DEPLOYMENT"        = "False"
     "ENABLE_ORYX_BUILD"                     = "True"
     "AZURE_KEY_VAULT_ENDPOINT"              = module.keyvault.AZURE_KEY_VAULT_ENDPOINT
---
+++ terraform/modules/postgresql_server/main.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_providers {
+    azurerm = {
+      version = "~>3.97.1"
+      source  = "hashicorp/azurerm"
+    }
+    azurecaf = {
+      source  = "aztfmod/azurecaf"
+      version = "~>1.2.24"
+    }
+  }
+}
+
+resource "azurecaf_name" "db_acc_name" {
+  name          = var.resource_token
+  resource_type = "azurerm_postgresql_server"
+  random_length = 0
+  clean_input   = true
+}
+
+resource "random_string" "username" {
+  length = 16
+  special = false
+}
+
+resource "random_password" "password" {
+  length = 16
+}
+
+resource "azurerm_postgresql_server" "postgresserver" {
+  name                = azurecaf_name.db_acc_name.result
+  location            = var.location
+  resource_group_name = var.rg_name
+ 
+  sku_name = "B_Gen5_2"
+ 
+  storage_mb                   = 5120
+  backup_retention_days        = 7
+  geo_redundant_backup_enabled = false
+  auto_grow_enabled            = true
+ 
+  administrator_login          = random_string.username.result
+  administrator_login_password = random_password.password.result
+  version                      = "9.5"
+  ssl_enforcement_enabled      = true
+
+  tags                         = var.tags
+}
+
+resource "azurerm_postgresql_database" "db" {
+  for_each            = toset(var.db_names)
+  name                = each.value
+  resource_group_name = var.rg_name
+  server_name         = azurerm_postgresql_server.postgresserver.name
+  charset             = "UTF8"
+  collation           = "en-GB"
+}
